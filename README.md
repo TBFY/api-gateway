f1b77dfe6384c13c7ac61b865f0b8319f74aa3d2
Carlos Badenes <cbadenes@gmail.com>
2019-12-16 12:10:39 +0100

    basic configuration

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# api-gateway
+<p align="center"><img width=50% src="https://github.com/TBFY/general/blob/master/figures/tbfy-logo.png"></p>
+<p align="center"><img width=40% src="https://github.com/TBFY/api-gateway/blob/master/logo.png"></p>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+![Docker](https://img.shields.io/badge/docker-v3+-blue.svg)
+![Docker-Compose](https://img.shields.io/badge/docker_compose-v3.0+-blue.svg)
+[![Release Status](https://jitci.com/gh/TBFY/api-gateway/svg)](https://jitci.com/gh/TBFY/api-gateway)
+[![GitHub Issues](https://img.shields.io/github/issues/TBFY/api-gateway.svg)](https://github.com/TBFY/api-gateway/issues)
+[![License](https://img.shields.io/badge/license-Apache2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+## Basic Overview
+
 Orchestration Microservice API Gateway. It provides a flexible abstraction layer and a single entry point that securely manages communication between TBFY clients and online tools via API.
+
+## Quick Start
+
+1. Install [Docker](https://docs.docker.com/install/) and [Docker-Compose](https://docs.docker.com/compose/install/)
+1. Clone this repo
+
+	```
+	git clone https://github.com/TBFY/api-gateway.git
+	```
+1. Run the API-Gateway by:
+    ```
+    docker-compose up -d
+	``` 
+1. It should be available at: [http://localhost:8000](http://localhost:8000)
+1. Load the services available in TBFY into the API-Gateway by:
+    ```
+    python3 src/main/python/config.py
+	``` 
+1. That's all!
+
+## Current Services
+
+Once the API-Gateway is up and running, you can request all services from a single entry point by different resources:
+
+|               service                      |                                 resource                                           |
+|--------------------------------------------|------------------------------------------------------------------------------------|
+|    reconciliation: organization names      |    [/brands](http://localhost:8000/brands)      |
+|    knowledge-graph: SparQL Queries         |    [/sparql](http://localhost:8000/sparql)      |
+|    search-API: documents                   |    [/documents](http://localhost:8000/documents)      |
+|    knowledge-graph-API: organizations      |    [/organizations](http://localhost:8000/organizations)      |
+|    knowledge-graph-API: contracts          |    [/contracts](http://localhost:8000/contracts)      |
+|    knowledge-graph-API: tenders            |    [/tenders](http://localhost:8000/tenders)      |
+|    knowledge-graph-API: awards             |    [/awards](http://localhost:8000/awards)      |
+|    knowledge-graph-API: contracting processes      |    [/contractingProcesses](http://localhost:8000/contractingProcesses)      |
+
+
+## Contributing
+Please take a look at our [contributing](https://github.com/TBFY/general/blob/master/guides/how-to-contribute.md) guidelines if you're interested in helping!
---
+++ src/main/python/config.py
@@ -0,0 +1,142 @@
+# Step one for every Python app that talks over the web
+#pip install requests
+
+import requests
+import json
+
+endpoint = "localhost:8001"
+
+reconciliationAPI = {
+    "name":"reconciliation-api",
+    "uris":"/brands",
+    "methods":"GET",
+    "upstream_url":"https://opencorporates.com/reconcile",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+sparQLEndpoint = {
+    "name":"sparql-endpoint",
+    "uris":"/sparql",
+    "methods":"GET",
+    "upstream_url":"http://data.tbfy.eu/repositories/TBFY",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+searchAPI = {
+    "name":"search-documents",
+    "uris":"/documents",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/search-api/documents",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+kgAPI_organisation = {
+    "name":"kg-organizations",
+    "uris":"/organizations",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/kg-api/organisation",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+kgAPI_contract = {
+    "name":"kg-contract",
+    "uris":"/contracts",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/kg-api/contract",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+kgAPI_award = {
+    "name":"kg-award",
+    "uris":"/awards",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/kg-api/award",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+kgAPI_tender = {
+    "name":"kg-tender",
+    "uris":"/tenders",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/kg-api/tender",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+kgAPI_contractingProcesses = {
+    "name":"kg-contractingProcesses",
+    "uris":"/contractingProcesses",
+    "methods":"GET",
+    "upstream_url":"http://tbfy.librairy.linkeddata.es/kg-api/contracting-process",
+    "strip_uri":True,
+    "preserve_host":False,
+    "retries":5,
+    "upstream_connect_timeout":60000,
+    "upstream_send_timeout":60000,
+    "upstream_read_timeout":60000,
+    "https_only":False,
+    "http_if_terminated":True
+}
+
+
+
+
+configs = [searchAPI, kgAPI_organisation, kgAPI_award, kgAPI_contract, kgAPI_contractingProcesses, kgAPI_tender, reconciliationAPI, sparQLEndpoint]
+
+
+for config in configs:
+    resp = requests.post('http://'+endpoint+'/apis', json=config)
+    if resp.status_code == 409:
+        print('{} already exists'.format(config["name"]))
+    elif resp.status_code == 201:
+        print('Added {} endpoint as route-service: {}'.format(config["name"],resp.json()["id"]))
+    else:
+        raise Exception('error','Error adding {} by POST /apis/ {}'.format(config["name"], resp.status_code))
